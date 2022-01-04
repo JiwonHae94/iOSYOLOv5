7e6577e3d5fe265945dca3d3111286e1d4af837d
JiwonHae <hjiwon1994@gmail.com>
2022-01-04 23:10:19 +0900

    FEAT : add frame manager and camera manager to handle incoming frames

---
+++ iOSYOLOv5/Camera/FrameManager.swift
@@ -0,0 +1,41 @@
+//
+//  FrameManager.swift
+//  iOSYOLOv5
+//
+//  Created by Jiwon_Hae on 2022/01/04.
+//
+
+import Foundation
+import AVFoundation
+
+class FrameManager: NSObject, ObservableObject{
+    static var shared = FrameManager()
+    
+    @Published var current : CVPixelBuffer?
+    
+    let videoOutputQueue = DispatchQueue(
+        label: "iOSCameraApp.VideoOutputQ",
+        qos: .userInitiated,
+        attributes: [],
+        autoreleaseFrequency: .workItem)
+    
+    private override init(){
+        super.init()
+        
+        CameraManager.shared.set(self, queue: videoOutputQueue)
+    }
+}
+
+extension FrameManager: AVCaptureVideoDataOutputSampleBufferDelegate{
+    func captureOutput(
+        _ output : AVCaptureOutput,
+        didOutput sampleBuffer : CMSampleBuffer,
+        from connection : AVCaptureConnection
+    ){
+        if let buffer = sampleBuffer.imageBuffer{
+            DispatchQueue.main.async {
+                self.current = buffer
+            }
+        }
+    }
+}
